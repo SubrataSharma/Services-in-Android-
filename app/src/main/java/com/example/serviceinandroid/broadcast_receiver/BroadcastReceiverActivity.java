b7a198b734541c676e5afc302e39f8774332cf69
SubrataSharma <sharmasubrata45@gmail.com>
2020-12-27 07:24:23 +0530

    #3 DYNAMIC RECEIVERS in foreground

--- app/src/main/java/com/example/serviceinandroid/broadcast_receiver/BroadcastReceiverActivity.java
+++ app/src/main/java/com/example/serviceinandroid/broadcast_receiver/BroadcastReceiverActivity.java
@@ -2,15 +2,30 @@ package com.example.serviceinandroid.broadcast_receiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 
 import com.example.serviceinandroid.R;
 
 public class BroadcastReceiverActivity extends AppCompatActivity {
-
+ExampleBroadcastReceiver broadcastReceiver = new ExampleBroadcastReceiver();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_broadcast_receiver);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter=new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(broadcastReceiver,intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(broadcastReceiver);
     }
 }
